5511fd6301097b489f1b134c74573158320b80a0
luii2606 <valemidx@gmail.com>
2025-05-09 12:22:33 -0500

    Diseño de los formularios

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,17 @@
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "LilJulian",
-  password: "julidar123",
-  database: "apiform"
-});
+// const connection = await mysql.createConnection({
+//   host: "localhost",
+//   user: "LilJulian",
+//   password: "julidar123",
+//   database: "apiform"
+// }); 
 
-export default connection;
+const conection =await mysql.createConnection({
+  host: "localhost",
+  user:"valemidx",
+  password:"luisa123",
+  database:"apiform"
+})
+
+export default conection;
